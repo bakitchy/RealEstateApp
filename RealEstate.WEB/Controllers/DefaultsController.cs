650fa477f2973f814f64424239fe68acc3968f59
bakitchy <bakitchy@gmail.com>
2024-03-03 15:54:51 +0300

    .....

---
+++ RealEstate.WEB/Controllers/DefaultsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RealEstate.WEB.Controllers
+{
+    public class DefaultsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ RealEstate.WEB/ViewComponents/_HeadViewComponentsPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RealEstate.WEB.ViewComponents
+{
+    public class _HeadViewComponentsPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
