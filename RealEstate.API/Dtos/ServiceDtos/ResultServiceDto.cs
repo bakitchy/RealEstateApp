a7c7daf6bc63b28cca9afa5fce13381d57674ab8
bakitchy <bakitchy@gmail.com>
2024-03-11 16:30:18 +0300

    ...

---
+++ RealEstate.API/Dtos/ServiceDtos/ResultServiceDto.cs
@@ -0,0 +1,10 @@
+﻿namespace RealEstate.API.Dtos.ServiceDtos
+{
+    public class ResultServiceDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Status { get; set; }
+
+    }
+}
