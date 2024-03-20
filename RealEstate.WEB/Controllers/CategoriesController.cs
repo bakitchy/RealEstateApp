353bcda979861379615b684ce26076f34a64ea26
bakitchy <bakitchy@gmail.com>
2024-03-20 17:01:05 +0300

    into the admin panel : - added create category buttons

--- RealEstate.WEB/Controllers/CategoriesController.cs
+++ RealEstate.WEB/Controllers/CategoriesController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using RealEstate.WEB.Dtos.CategoryDtos;
+using System.Text;
 
 namespace RealEstate.WEB.Controllers
 {
@@ -25,5 +26,27 @@ namespace RealEstate.WEB.Controllers
             }
             return View();
         }
+
+        [HttpGet]
+        public IActionResult CreateCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryDto createCategoryDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createCategoryDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:44359/api/Categories", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
     }
 }
